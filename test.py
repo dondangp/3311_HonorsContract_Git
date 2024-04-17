610300ba26231615174ffd1a10e97c45068d4385
Don Dang <dondang@Dons-MacBook-Pro.local>
2024-04-17 15:06:37 -0500

    commitd

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-x = "Water"
-y = "Soda"
-temp = x
-print("Initial x : " + x)
-print("Initial y :" + x)
-x = y
-y = temp
-print("x : "+ x)
-print("y : " + y)
-print("temp: " + temp)
+print("Hello World")
